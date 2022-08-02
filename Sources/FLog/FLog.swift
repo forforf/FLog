0f3bcabf73dda8c362e3d125e68ed48d6268efa5
forforf <dmarti21@gmail.com>
2022-08-02 07:25:27 -0400

    Working implementation

--- Sources/FLog/FLog.swift
+++ Sources/FLog/FLog.swift
@@ -1,6 +1,105 @@
-public struct FLog {
-    public private(set) var text = "Hello, World!"
+import Foundation
+import os.log
 
-    public init() {
+protocol FLoggable {
+    init(subsystem: String, category: String)
+    func debug(_ s:String)
+    func info(_ s:String)
+    func notice(_ s:String)
+    func warning(_ s:String)
+    func error(_ s:String)
+}
+
+// wrapper around Logger to conform to Loggable
+// FLoggable allows us to use different logging implementations (and mocking for tests)
+@available(iOS 14.0, *)
+struct FLogger: FLoggable {
+    let logger: Logger
+    
+    init(subsystem: String, category: String) {
+        self.logger = Logger(subsystem: subsystem, category: category)
+    }
+    
+    func debug(_ s: String) { logger.debug("\(s)")}
+    func info(_ s: String) { logger.info("\(s)")}
+    func notice(_ s: String) { logger.notice("\(s)")}
+    func warning(_ s: String) { logger.warning("\(s)")}
+    func error(_ s: String) { logger.error("\(s)")}
+}
+
+enum FLogLevel: String {
+    case debug = "🔵[debug] "
+    case info = "🟢[info] "
+    case notice = "🟡[notice] "
+    case warning = "🟠[warning] "
+    case error = "🔴[error] "
+    
+    func pretty(_ message: String) -> String {
+        return "\(self.rawValue)\(message)"
     }
 }
+
+
+/**
+  See ``FLog`` for the typical usage.
+ */
+@available(iOS 14.0, *)
+struct FLogBase {
+    public static let subsystem = Bundle.main.bundleIdentifier ?? "FLog"
+    public static let category = "FLog"
+    
+    let logger: FLoggable
+    
+    
+    func debug(_ message: String) {
+        logger.debug(FLogLevel.debug.pretty(message))
+    }
+    
+    func info(_ message: String) {
+        logger.info(FLogLevel.info.pretty(message))
+    }
+    
+    func notice(_ message: String) {
+        logger.notice(FLogLevel.notice.pretty(message))
+    }
+    
+    func warning(_ message: String) {
+        logger.warning(FLogLevel.warning.pretty(message))
+    }
+    
+    func error(_ message: String) {
+        logger.error(FLogLevel.error.pretty(message))
+    }
+
+
+    public init(subsystem: String = subsystem, category: String = category) {
+        self.init(subsystem: subsystem, category: category, loggerType: FLogger.self)
+    }
+    
+    public init(subsystem: String = subsystem, category: String = category, loggerType: FLoggable.Type) {
+        self.logger = loggerType.init(subsystem: subsystem, category: category)
+    }
+}
+
+/**
+Extending Logger so we can format the messages
+ 
+Typical Usage
+ 
+```
+class MyClass {
+    let log = FLog<MyClass>.make()
+ 
+        // ... then to log
+        Self.log.debug("LogMe")
+}
+```
+*/
+@available(iOS 14.0, *)
+struct FLog<T>{
+    static func make() -> FLogBase {
+        let logName = String(describing: T.self as Any)
+        return FLogBase(category: logName)
+    }
+}
+
--- Tests/FLogTests/FLogTests.swift
+++ Tests/FLogTests/FLogTests.swift
@@ -1,11 +1,123 @@
 import XCTest
 @testable import FLog
 
+struct LoggerAssert {
+    var debug: (_ s: String) -> Void = { s in XCTAssert(true) }
+    var info: (_ s: String) -> Void = { s in XCTAssert(true) }
+    var notice: (_ s: String) -> Void = { s in XCTAssert(true) }
+    var warning: (_ s: String) -> Void = { s in XCTAssert(true) }
+    var error: (_ s: String) -> Void = { s in XCTAssert(true) }
+}
+
+struct LoggerMock: FLoggable {
+    
+    static var assert = LoggerAssert()
+    let subsystem: String
+    let category: String
+    
+    
+    init(subsystem: String, category: String) {
+        self.subsystem = subsystem
+        self.category = category
+    }
+    
+    func debug(_ s: String) { LoggerMock.assert.debug(s) }
+    func info(_ s: String) { LoggerMock.assert.info(s) }
+    func notice(_ s: String) { LoggerMock.assert.notice(s) }
+    func warning(_ s: String) { LoggerMock.assert.warning(s) }
+    func error(_ s: String) { LoggerMock.assert.error(s) }
+}
+
+@available(iOS 14.0, *)
+class ClassToLog {
+    static let log = FLog<ClassToLog>.make()
+}
+
+@available(iOS 14.0, *)
 final class FLogTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(FLog().text, "Hello, World!")
+    
+    func test_FLog_is_logger() throws {
+        let logger = FLog<FLogTests>.make()
+        logger.debug("Test")
+        XCTAssertEqual(String(describing: type(of: logger.logger)), "FLogger")
+    }
+    
+    func test_FLog_make_subsystem() throws {
+        let logger = FLog<FLogTests>.make()
+        let subsystem = Bundle.main.bundleIdentifier
+        XCTAssertEqual(type(of: logger).subsystem, subsystem)
+    }
+    
+    func test_FLog_make_category() throws {
+        let logger = FLog<FLogTests>.make()
+        let category = "FLog"
+        XCTAssertEqual(type(of: logger).category, category)
+    }
+    
+    // TODO: Figure out a way to test formatting that doesn't complicate the base class
+    func test_FLog_debug_format() throws {
+        let message = "Test debug message."
+        let logEntry = "🔵[debug] \(message)"
+        // override the debug assert
+        let loggerAssert = LoggerAssert(debug: {s in XCTAssertEqual(s, logEntry) })
+        LoggerMock.assert = loggerAssert
+        let flogger = FLogBase(subsystem: "Subsystem", category: "Category", loggerType: LoggerMock.self)
+        // Uncomment when modifying code to be sure to catch false positives
+        // XCTExpectFailure("Expected failure to prevent false positives") {
+        //    flogger.debug("blah")
+        // }
+        flogger.debug(message)
+    }
+    
+    func test_FLog_info_format() throws {
+        let message = "Test info message."
+        let logEntry = "🟢[info] \(message)"
+        let loggerAssert = LoggerAssert(info: {s in XCTAssertEqual(s, logEntry) })
+        LoggerMock.assert = loggerAssert
+        let flogger = FLogBase(subsystem: "Subsystem", category: "Category", loggerType: LoggerMock.self)
+        // Uncomment when modifying code to be sure to catch false positives
+        // XCTExpectFailure("Expected failure to prevent false positives") {
+        //     flogger.info("blah")
+        // }
+        flogger.info(message)
+    }
+    
+    func test_FLog_notice_format() throws {
+        let message = "Test notice message."
+        let logEntry = "🟡[notice] \(message)"
+        let loggerAssert = LoggerAssert(notice: {s in XCTAssertEqual(s, logEntry) })
+        LoggerMock.assert = loggerAssert
+        let flogger = FLogBase(subsystem: "Subsystem", category: "Category", loggerType: LoggerMock.self)
+        // Uncomment when modifying code to be sure to catch false positives
+        // XCTExpectFailure("Expected failure to prevent false positives") {
+        //     flogger.notice("blah")
+        // }
+        flogger.notice(message)
+    }
+    
+    func test_FLog_warning_format() throws {
+        let message = "Test warning message."
+        let logEntry = "🟠[warning] \(message)"
+        let loggerAssert = LoggerAssert(warning: {s in XCTAssertEqual(s, logEntry) })
+        LoggerMock.assert = loggerAssert
+        let flogger = FLogBase(subsystem: "Subsystem", category: "Category", loggerType: LoggerMock.self)
+        // Uncomment when modifying code to be sure to catch false positives
+        // XCTExpectFailure("Expected failure to prevent false positives") {
+        //     flogger.warning("blah")
+        // }
+        flogger.warning(message)
+    }
+    
+    func test_FLog_error_format() throws {
+        let message = "Test info message."
+        let logEntry = "🔴[error] \(message)"
+        let loggerAssert = LoggerAssert(error: {s in XCTAssertEqual(s, logEntry) })
+        LoggerMock.assert = loggerAssert
+        let flogger = FLogBase(subsystem: "Subsystem", category: "Category", loggerType: LoggerMock.self)
+        // Uncomment when modifying code to be sure to catch false positives
+        // XCTExpectFailure("Expected failure to prevent false positives") {
+        //     flogger.error("blah")
+        // }
+        flogger.error(message)
     }
 }
